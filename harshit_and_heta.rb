eeab0f6a6e13c37cfe10ec65e9e2958cbd96cb2b
Harshit Sanghvi <sanghvi.harshit@gmail.com>
2015-02-17 16:42:34 -0500

    Fixed issue with google analytics.

--- harshit_and_heta.rb
+++ harshit_and_heta.rb
@@ -1,14 +1,14 @@
 class HarshitAndHeta < Sinatra::Base
-  PREFERRED_HOST = 'harshitheta.us'.freeze
-  WEDDING_DATE = Date.parse('2013-11-19').freeze
+  #PREFERRED_HOST = 'harshitheta.us'.freeze
+  #WEDDING_DATE = Date.parse('2013-11-19').freeze
 
-  if ENV['RACK_ENV'] == 'production'
-    before do
-      if request.host != PREFERRED_HOST
-        redirect to("http://#{PREFERRED_HOST + request.path_info}")
-      end
-    end
-  end
+  #if ENV['RACK_ENV'] == 'production'
+  #  before do
+  #     if request.host != PREFERRED_HOST
+  #       redirect to("http://#{PREFERRED_HOST + request.path_info}")
+  #     end
+  #   end
+  # end
 
   get '/' do
     # @days = (WEDDING_DATE - Date.today).to_i
